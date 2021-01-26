355b4e75f8914904ac3cf427398814fafc22c3cc
Joshua MacSween <joshmacsween@gmail.com>
2021-01-26 14:19:11 -0700

    CRUD Operations

--- aspnet-learn/src/ContosoPets.Api/Controllers/ProductsController.cs
+++ aspnet-learn/src/ContosoPets.Api/Controllers/ProductsController.cs
@@ -23,9 +23,58 @@ namespace ContosoPets.Api.Controllers
     public ActionResult<List<Product>> GetAll() =>
       _context.Products.ToList();
 
-      // Get ID
+      // Get by ID action
+      [HttpGet("{id}")]
+      public async Task<ActionResult<Product>> GetById(int id)
+      {
+        var product = await _context.Products.FindAsync(id);
+
+        if (product is null)
+        {
+          return NotFound();
+        }
+        return product;
+      }
+
       // Post
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+          _context.Products.Add(product);
+          await _context.SaveChangesAsync();
+          return CreatedAtAction(nameof(GetById), new { id = product.Id }, product);
+        }
+
       // Put
+      [HttpPut("{id}")]
+      public async Task<IActionResult> Update(int id, Product product)
+      {
+        if (id != product.Id)
+        {
+          return BadRequest();
+        }
+
+        _context.Entry(product).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+      }
+
       // Delete
+      [HttpDelete("{id}")]
+      public async Task<IActionResult> Delete(int id)
+      {
+        var product = await _context.Products.FindAsync(id);
+
+        if (product is null)
+        {
+          return NotFound();
+        }
+
+        _context.Products.Remove(product);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+      }
   }
 }
